743f5e1416bb4fadb21e5da6a46444bda9046648
Matthew Owens <mowens@redhat.com>
2019-07-12 01:32:13 -0600

    building pip package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name = "webplatform-cli",
-   version = "1.1.2",
+   version = "1.1.3",
    description = "CLI used for a webplatform",
    author = "Matthew Owens",
    author_email = "mowens@redhat.com",
