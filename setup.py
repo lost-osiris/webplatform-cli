162cdaeef85cb5f60c14fc9c5a3d92ae74bf9975
Matthew Owens <mowens@redhat.com>
2019-08-05 05:44:09 -0600

    forgot to update webplatform-cli version on base container image requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name = "webplatform-cli",
-   version = "1.2.0",
+   version = "1.2.1",
    description = "CLI used for a webplatform",
    author = "Matthew Owens",
    author_email = "mowens@redhat.com",
